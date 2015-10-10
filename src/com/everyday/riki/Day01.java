4465b5d694826d7d7e52f6ee091c223f725bcaa6
Riki <java_ljx@163.com>
2015-10-10 11:30:39 +0800

    test

--- src/com/everyday/riki/Day01.java
+++ src/com/everyday/riki/Day01.java
@@ -1,7 +1,13 @@
 package com.everyday.riki;
 
+import org.junit.Test;
+
 /**
  * Created by macmini on 15/10/10.
  */
 public class Day01 {
+    @Test
+    public void test() {
+        System.out.println(111);
+    }
 }
